2fb70754993eff5f19cbea6ab44dd6ab0551d77a
cartlex <ap26booking@gmail.com>
2023-11-15 23:33:42 +0300

    regroup imports

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -3,8 +3,7 @@ pragma solidity 0.8.21;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
-import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
